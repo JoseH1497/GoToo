2aac6bece59da463211ead5b8a305c61b5c7c797
Sergio Rosendo <sergio.rosendo333@gmail.com>
2018-03-28 17:20:26 -0700

    Xcode Base Project

---
+++ App/Go Too/Go Too/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Go Too
+//
+//  Created by Sergio Rosendo on 3/28/18.
+//  Copyright © 2018 Reactive Works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
