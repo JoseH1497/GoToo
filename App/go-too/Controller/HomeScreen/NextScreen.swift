21acca11ad9bed28b64d7c41454d04d22d823cdb
Jose Herrera <joseherrerna@Gabrielas-MacBook-Pro-2.local>
2018-04-16 13:50:27 -0700

    Textfile

---
+++ App/go-too/Controller/HomeScreen/NextScreen.swift
@@ -0,0 +1,20 @@
+//
+//  NextScreen.swift
+//  go-too
+//
+//  Created by Jose Herrera on 4/16/18.
+//  Copyright © 2018 Reactive Works. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class NextScreenController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+        
+    }
+
+}
+
+
