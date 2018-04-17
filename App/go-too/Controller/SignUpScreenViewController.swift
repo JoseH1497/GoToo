bf2ed38055e828d6306bd8b4085b439afe83e646
Sergio Rosendo <sergio.rosendo333@gmail.com>
2018-04-17 15:51:30 -0700

    Created Sign Up Screen View Controlelr Skeleton Code

--- App/go-too/Controller/SignUpScreenViewController.swift
+++ App/go-too/Controller/SignUpScreenViewController.swift
@@ -30,17 +30,54 @@ class SignUpScreenViewController: UIViewController {
     
     //:MARK: UI Elements
     @IBAction func user_name_entered(_ sender: UITextField) {
+        //:Textfield is not empty
+        if let user_name : String = sender.text {
+            //:Checking if username is already in use
+            
+        }
+        //:Textfield is empty
+        else{
+            registration.set_username_satisfied(status: false)
+        }
         
     }
     @IBAction func emailed_entered(_ sender: UITextField) {
+        //:Textfield is not empty
+        if let email : String = sender.text {
+            
+        }
+        //:Textfield is empty
+        else{
+            registration.set_email_satisfied(status: false)
+        }
     }
     
     @IBAction func password_entered(_ sender: UITextField) {
+        //:Textfield is not empty
+        if let password : String = sender.text {
+            
+        }
+            //:Textfield is empty
+        else{
+            registration.set_password_satisfied(status: false)
+        }
     }
     
     @IBAction func password_confirmation_entered(_ sender: UITextField) {
+        //:Textfield is not empty
+        if let password_confirmation : String = sender.text {
+            
+        }
+            //:Textfield is empty
+        else{
+            registration.set_password_confirmation_satisfied(status: false)
+        }
     }
     
     @IBAction func sign_up_button_pressed(_ sender: UIButton) {
+        //:Check if all registration conditions have been satisfied
+        if registration.registration_completed() {
+            
+        }
     }
 }
